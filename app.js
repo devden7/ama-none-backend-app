16baf6e9ef76ef0bfc0cdb48326aa589d3471d2a
devden7 <devdenn7@gmail.com>
2023-09-04 13:18:16 +0700

    update url allow origin

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const productRoutes = require("./routes/products.js");
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "http://localhost:3000/, https://amanone-app.vercel.app/"
+    "https://amanone-app.vercel.app/"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
